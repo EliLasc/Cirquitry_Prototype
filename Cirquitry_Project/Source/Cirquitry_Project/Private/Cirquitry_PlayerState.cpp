01f1a6584543f91e25732afaf435bc86516257a4
EliLas <Elijahlascurain95@gmail.com>
2025-05-18 19:39:48 -0700

    Cirquitry_PlayerState first created

---
+++ Cirquitry_Project/Source/Cirquitry_Project/Private/Cirquitry_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Cirquitry
+
+
+#include "Cirquitry_PlayerState.h"
+
+ACirquitry_PlayerState::ACirquitry_PlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
---
+++ Cirquitry_Project/Source/Cirquitry_Project/Public/Cirquitry_PlayerState.h
@@ -0,0 +1,18 @@
+// Copyright Cirquitry
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "Cirquitry_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CIRQUITRY_PROJECT_API ACirquitry_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	ACirquitry_PlayerState();
+};
